1942dd78ba22b5927d9285cc17a457211bbea9db
Григорий Ковалев <g-kovalev@list.ru>
2025-06-28 14:47:10 +0400

    shop-1-1: Создать таблицы в БД и Entity к ним
    # Conflicts:
    #	shop/src/main/java/ru/itone/jun2middle/shop/model/entity/Order.java
    #	shop/src/main/java/ru/itone/jun2middle/shop/model/entity/Product.java

--- shop/src/main/java/ru/itone/jun2middle/shop/model/entity/Order.java
+++ shop/src/main/java/ru/itone/jun2middle/shop/model/entity/Order.java
@@ -2,16 +2,11 @@ package ru.itone.jun2middle.shop.model.entity;
 
 import jakarta.persistence.*;
 import java.util.UUID;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name="orders")
 public class Order {

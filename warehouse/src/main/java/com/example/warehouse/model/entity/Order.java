12bcac82acad37be19ef4cbfb67744edf1ce7a94
a.kolobov <a.kolobov@astondevs.ru>
2025-06-28 13:34:48 +0700

    Создать таблицы в БД и Entity к ним

---
+++ warehouse/src/main/java/com/example/warehouse/model/entity/Order.java
@@ -0,0 +1,34 @@
+package com.example.warehouse.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    private UUID shop_order_id;
+
+    @NotNull
+    private UUID product_id;
+
+}

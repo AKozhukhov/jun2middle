83f4f501fd654bc7121fb80ad8501cf8fe97b281
VVBoss <growder1@gmail.com>
2025-07-05 19:18:07 +0500

    Delivery-2-2, Сервис с реализацией статуса доставки

--- delivery/src/main/java/com/example/delivery/repository/OrderRepository.java
+++ delivery/src/main/java/com/example/delivery/repository/OrderRepository.java
@@ -1,11 +1,16 @@
 package com.example.delivery.repository;
 
 import com.example.delivery.entity.Order;
+import com.example.delivery.entity.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-    List<Order> findAllByOrderByCreateDateAsc();
+
+    List<Order> findByStatusOrderByCreateDateAsc(OrderStatus status);
+
+    List<Order> findByStatusAndArrivalTimeLessThanEqual(OrderStatus orderStatus, LocalDateTime now);
 }
--- delivery/src/main/java/com/example/delivery/service/CourierService.java
+++ delivery/src/main/java/com/example/delivery/service/CourierService.java
@@ -4,7 +4,6 @@ import com.example.delivery.entity.Courier;
 import com.example.delivery.repository.CourierRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,20 +23,21 @@ public class CourierService {
     }
 
     @Transactional
-    public void assignOrderToCourier(Courier courier, int minutes) {
+    public void assignOrderToCourier(Courier courier, int seconds) {
         courier.setBusy(true);
-        courier.setBusyUntil(LocalDateTime.now().plusMinutes(minutes));
+        courier.setBusyUntil(LocalDateTime.now().plusSeconds(seconds));
         courierRepository.save(courier);
         log.debug("Курьер {} занят до {}", courier.getId(), courier.getBusyUntil());
     }
 
-    @Scheduled(fixedRate = 60000) //Ну тут вопрос в том, а реально важно, что бы прям на 3 минуты с отправки, или почти 4 минуты тоже подходит.
-    //Ибо иначе на каждого вызванного курьера надо будет создавать отдельный Schedule, что звучит не очень
-    @Transactional
-    public void releaseExpiredCouriers() {
-        int released = courierRepository.releaseExpiredCouriers();
-        if (released > 0) {
-            log.debug("Освобождено {} курьеров", released);
-        }
-    }
+//    Отключено за ненадобностью
+//    @Scheduled(fixedRate = 60000) //Ну тут вопрос в том, а реально важно, что бы прям на 3 минуты с отправки, или почти 4 минуты тоже подходит.
+//    //Ибо иначе на каждого вызванного курьера надо будет создавать отдельный Schedule, что звучит не очень
+//    @Transactional
+//    public void releaseExpiredCouriers() {
+//        int released = courierRepository.releaseExpiredCouriers();
+//        if (released > 0) {
+//            log.debug("Освобождено {} курьеров", released);
+//        }
+//    }
 }

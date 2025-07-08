41f6ac36b4f55fc988bea813aceb9607a03db5bb
Григорий Ковалев <g-kovalev@list.ru>
2025-07-08 15:47:46 +0400

    shop-2-4: Вывод неоценённых товаров, проставление цены, запрет заказа с неоценённым товаром

--- shop/src/main/java/ru/itone/jun2middle/shop/controller/ProductController.java
+++ shop/src/main/java/ru/itone/jun2middle/shop/controller/ProductController.java
@@ -1,12 +1,13 @@
 package ru.itone.jun2middle.shop.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.itone.jun2middle.shop.facade.ProductFacade;
 import ru.itone.jun2middle.shop.model.dto.product.ProductDto;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/products")
@@ -14,8 +15,36 @@ import ru.itone.jun2middle.shop.model.dto.product.ProductDto;
 public class ProductController {
     private final ProductFacade productFacade;
 
+    /**
+     * Добавляет новый продукт
+     *
+     * @param productDto объект ProductDto
+     * @return созданный ProductDto
+     */
     @PostMapping
     public ProductDto create(@RequestBody ProductDto productDto) {
         return productFacade.create(productDto);
+    }
+
+    /**
+     * Выводит все неоценённые товары
+     *
+     * @return List из неоценённых товаров
+     */
+    @GetMapping
+    public List<ProductDto> getUnpriced() {
+        return productFacade.getUnpriced();
+    }
+
+    /**
+     * Устанавливает цену для товара
+     *
+     * @param id ID товара
+     * @param price новая цена
+     * @return ProductDto с проставленной ценой
+     */
+    @PutMapping
+    public ProductDto setPrice(@RequestParam UUID id, @RequestParam BigDecimal price) {
+        return productFacade.setPrice(id,price);
     }
 }

61e80391ac0cc459234c5eb27e5f3f72ef896d55
a.kolobov <a.kolobov@astondevs.ru>
2025-07-11 17:15:38 +0700

    поправил обработку ответа из магазина

--- warehouse/src/main/java/com/example/warehouse/client/ShopClient.java
+++ warehouse/src/main/java/com/example/warehouse/client/ShopClient.java
@@ -4,8 +4,9 @@ import com.example.warehouse.exception.ProductException;
 import com.example.warehouse.model.dto.CreatedProductDto;
 import com.example.warehouse.model.dto.ShopProductRequest;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestClient;
 
 @Service
@@ -21,13 +22,13 @@ public class ShopClient {
                 .name(createdProductDto.getName())
                 .size(createdProductDto.getSize())
                 .build();
-        ResponseEntity<ShopProductRequest> response = restClient
-                .post()
-                .uri("/api/v1/products")
-                .body(shopProductRequest)
-                .retrieve()
-                .toEntity(ShopProductRequest.class);
-        if (!response.getStatusCode().is2xxSuccessful()) {
+        try {
+            restClient.post()
+                    .uri("/api/v1/products")
+                    .body(shopProductRequest)
+                    .retrieve()
+                    .toEntity(ShopProductRequest.class);
+        } catch (HttpServerErrorException | HttpClientErrorException e){
             throw new ProductException("Bad request to Shop. Product name = %s".formatted(createdProductDto.getName()));
         }
     }
